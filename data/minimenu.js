b973cb2b4eac6c281cb4105bb1b6182505076e0c
Zmaty <matypaur@gmail.com>
2026-05-18 12:18:02 +0200

    Add files via upload

--- data/minimenu.js
+++ data/minimenu.js
@@ -1,6 +1,16 @@
 // optimalizace pro mobily
 let menu = document.querySelector('#menu');
-document.querySelector('.minimenu').onclick = function(){
+let miniMenu = document.querySelector('.minimenu');
+
+miniMenu.onclick = function () {
 this.classList.toggle('active');
 menu.classList.toggle('active');
-}
+};
+
+// zavření po kliknutí na položku menu
+document.querySelectorAll('#menu a').forEach(link => {
+    link.onclick = () => {
+        miniMenu.classList.remove('active');
+        menu.classList.remove('active');
+    };
+});
